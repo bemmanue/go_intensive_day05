0c58424ff506c13a7f9a69fcedffc05657f8b770
Burma Emmanuel <bemmanue@si-g4.kzn.21-school.ru>
2022-11-28 18:21:25 +0300

    ex00 is done

--- ex00/main.go
+++ ex00/main.go
@@ -1,27 +1,61 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 )
 
 type TreeNode struct {
-	HasToy	bool
-	Left	*TreeNode
-	Right	*TreeNode
+	HasToy bool
+	Left   *TreeNode
+	Right  *TreeNode
+}
+
+func countToys(root *TreeNode, count *int) {
+	if root == nil {
+		return
+	}
+
+	if root.HasToy == true {
+		*count += 1
+	}
+
+	countToys(root.Left, count)
+	countToys(root.Right, count)
 }
 
 func areToysBalanced(root *TreeNode) (bool, error) {
-	return true, nil
+	var leftBranchToys int
+	var rightBranchToys int
+
+	if root == nil {
+		return false, errors.New("root node is nil")
+	}
+
+	countToys(root.Left, &leftBranchToys)
+	countToys(root.Right, &rightBranchToys)
+
+	if leftBranchToys == rightBranchToys {
+		return true, nil
+	} else {
+		return false, nil
+	}
 }
 
 func main() {
 	var root TreeNode
 
-	res, err := areToysBalanced(&root)
-	if err != nil {
-		log.Fatal(err)
-	}
+	root.HasToy = true
+	root.Left = &TreeNode{HasToy: true}
+	root.Left.Left = &TreeNode{HasToy: true}
+	root.Left.Right = &TreeNode{HasToy: false}
+	root.Right = &TreeNode{HasToy: true}
+	root.Right.Left = &TreeNode{HasToy: true}
 
-	fmt.Println(res)
+	garland, err := areToysBalanced(&root)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(garland)
 }
