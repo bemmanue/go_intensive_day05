36a6d506c6cfbcd5e8537fcbe5fe66276e1e42d5
Burma Emmanuel <bemmanue@si-g4.kzn.21-school.ru>
2022-11-28 19:20:32 +0300

    ex01 is done

--- ex01/main.go
+++ ex01/main.go
@@ -1,1 +1,100 @@
-package ex01
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type TreeNode struct {
+	HasToy bool
+	Left   *TreeNode
+	Right  *TreeNode
+}
+
+func fromRightToLeft(queue []TreeNode) []TreeNode {
+	var nextQueue []TreeNode
+
+	for len(queue) != 0 {
+		x := queue[len(queue)-1]
+		queue = queue[:len(queue)-1]
+
+		if x.Right != nil {
+			nextQueue = append(nextQueue, *x.Right)
+		}
+
+		if x.Left != nil {
+			nextQueue = append(nextQueue, *x.Left)
+		}
+	}
+
+	return nextQueue
+}
+
+func fromLeftToRight(queue []TreeNode) []TreeNode {
+	var nextQueue []TreeNode
+
+	for len(queue) != 0 {
+		x := queue[len(queue)-1]
+		queue = queue[:len(queue)-1]
+
+		if x.Left != nil {
+			nextQueue = append(nextQueue, *x.Left)
+		}
+
+		if x.Right != nil {
+			nextQueue = append(nextQueue, *x.Right)
+		}
+	}
+
+	return nextQueue
+}
+
+func getGarland(queue []TreeNode) []bool {
+	var garland []bool
+
+	for _, q := range queue {
+		garland = append(garland, q.HasToy)
+	}
+
+	return garland
+}
+
+func unrollGarland(root *TreeNode) ([]bool, error) {
+	var queue []TreeNode
+	var garland []bool
+
+	queue = append(queue, *root)
+
+	for {
+		garland = append(garland, getGarland(queue)...)
+		queue = fromLeftToRight(queue)
+		if queue == nil {
+			break
+		}
+
+		garland = append(garland, getGarland(queue)...)
+		queue = fromRightToLeft(queue)
+		if queue == nil {
+			break
+		}
+	}
+	return garland, nil
+}
+
+func main() {
+	var root TreeNode
+
+	root.HasToy = true
+	root.Left = &TreeNode{HasToy: true}
+	root.Left.Left = &TreeNode{HasToy: true}
+	root.Left.Right = &TreeNode{HasToy: false}
+	root.Right = &TreeNode{HasToy: false}
+	root.Right.Left = &TreeNode{HasToy: true}
+	root.Right.Right = &TreeNode{HasToy: true}
+
+	garland, err := unrollGarland(&root)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(garland)
+}
